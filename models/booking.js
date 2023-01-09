d9952d350f002018a74a6cd133e85b373ad561fc
Home <syaddad.snfa@gmail.com>
2023-01-09 22:17:54 +0700

    fixing bugs

--- models/booking.js
+++ models/booking.js
@@ -18,6 +18,10 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false
         },
+        id_type_ws: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         start_date: {
             type: DataTypes.STRING,
             allowNull: false
